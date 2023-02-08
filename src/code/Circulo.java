a9975e18c608570a7c6ac894734e0141a06332e8
osvi <joseconstantino987@gmail.com>
2023-02-07 23:53:31 -0600

    se ha agregado la table

--- src/code/Circulo.java
+++ src/code/Circulo.java
@@ -11,15 +11,20 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.table.DefaultTableModel;
 
 public class Circulo extends JFrame{
     private JPanel panel;
-    private JLabel x,y,h,k,r;
-    private JTextField X,Y,H,K;
+    private JLabel h,k,r, inicio, fin;
+    private JTextField H,K, campoInicio, campoFin;
     private JCheckBox fueraOrigen;
     private JButton calcular, regresar; 
-    private double resultado;
+    private JTable resultados; 
+    DefaultTableModel modelo;
 
     public Circulo(){
         super("Circulo");
@@ -36,6 +41,7 @@ public class Circulo extends JFrame{
         agregarInputText();
         agregarCheckBox();
         agregarBotones();
+        
     } 
     public void agregarPanel(){
         panel = new JPanel();
@@ -43,25 +49,25 @@ public class Circulo extends JFrame{
         this.add(panel);
     } 
     public void agregarLabels(){
-        x = new JLabel("X: ");
-        x.setFont(new Font("Century Gothic", Font.PLAIN, 20));
-        x.setBounds(20,50,50,20);
-        panel.add(x);
+        inicio = new JLabel("Inicio: ");
+        inicio.setFont(new Font("Century Gothic", Font.PLAIN, 20));
+        inicio.setBounds(20,50,80,20);
+        panel.add(inicio);
 
-        y = new JLabel("Y: ");
-        y.setFont(new Font("Century Gothic", Font.PLAIN, 20));
-        y.setBounds(300,50,50,20);
-        panel.add(y);
+        fin = new JLabel("Final: ");
+        fin.setFont(new Font("Century Gothic", Font.PLAIN, 20));
+        fin.setBounds(300,50,80,20);
+        panel.add(fin);
 
         h = new JLabel("h: ");
         h.setFont(new Font("Century Gothic", Font.PLAIN, 20));
-        h.setBounds(150,50,50,20);
+        h.setBounds(180,50,50,20);
         h.setVisible(false);
         panel.add(h);
 
         k = new JLabel("k: ");
         k.setFont(new Font("Century Gothic", Font.PLAIN, 20));
-        k.setBounds(420,50,50,20);
+        k.setBounds(450,50,50,20);
         k.setVisible(false);
         panel.add(k);
 
@@ -73,22 +79,22 @@ public class Circulo extends JFrame{
 
     }
     public void agregarInputText(){
-        X = new JTextField();
-        X.setBounds(50, 50,70,25);
-        panel.add(X); 
+        campoInicio = new JTextField();
+        campoInicio.setBounds(90, 50,70,25);
+        panel.add(campoInicio); 
 
-        Y = new JTextField();
-        Y.setBounds(330,50,70,25);
-        panel.add(Y);
+        campoFin = new JTextField();
+        campoFin.setBounds(360,50,70,25);
+        panel.add(campoFin);
 
         H = new JTextField();
-        H.setBounds(170,50,70,25);
+        H.setBounds(200,50,70,25);
         H.setVisible(false);
         panel.add(H);
         
 
         K = new JTextField();
-        K.setBounds(450,50,70,25);
+        K.setBounds(480,50,70,25);
         K.setVisible(false);
         panel.add(K);
     }
@@ -113,31 +119,31 @@ public class Circulo extends JFrame{
     }
     private void accionesBotones(){ 
         ActionListener evento1 = new ActionListener(){
-            
+        int aux1, aux2;    
             @Override
             public void actionPerformed(ActionEvent e){    
                 try{
-                    if(X.getText().length() !=  0 && Y.getText().length() != 0 && !fueraOrigen.isSelected()){
+                    if(campoInicio.getText().length() !=  0 && campoFin.getText().length() != 0 && !fueraOrigen.isSelected()){
                         if(e.getSource() == calcular){
-                            resultado = redondear(Math.sqrt(Math.pow(Double.parseDouble(X.getText()),2) + Math.pow(Double.parseDouble(Y.getText()),2)));
-                            r.setText(("Centro (0,0)         r: "+resultado));
-                            r.setVisible(true);
+                            aux1 = (Integer.parseInt(campoInicio.getText()) < Integer.parseInt(campoFin.getText())) ? Integer.parseInt(campoInicio.getText()) : Integer.parseInt(campoFin.getText());
+                            aux2 = (Integer.parseInt(campoFin.getText()) > Integer.parseInt(campoInicio.getText())) ? Integer.parseInt(campoFin.getText()) : Integer.parseInt(campoInicio.getText());
+                            agregarTabla(1, aux1, aux2);
                         }    
                     } 
-                    else if((X.getText().length() !=  0 && Y.getText().length() != 0) && (H.getText().length() !=  0 && K.getText().length() != 0)){
+                    else if((campoInicio.getText().length() !=  0 && campoFin.getText().length() != 0) && (H.getText().length() !=  0 && K.getText().length() != 0)){
                         if(e.getSource() == calcular){
-                           
-                            resultado = redondear(Math.sqrt(Math.pow(Double.parseDouble(X.getText()) - Double.parseDouble(H.getText()),2) 
-                            + Math.pow(Double.parseDouble(Y.getText()) - Double.parseDouble(K.getText()),2)));
-                            r.setText(("Centro ("+H.getText()+","+K.getText()+")         r: "+resultado));
-                            r.setVisible(true);
+                            aux1 = (Integer.parseInt(campoInicio.getText()) < Integer.parseInt(campoFin.getText())) ? Integer.parseInt(campoInicio.getText()) : Integer.parseInt(campoFin.getText());
+                            aux2 = (Integer.parseInt(campoFin.getText()) > Integer.parseInt(campoInicio.getText())) ? Integer.parseInt(campoFin.getText()) : Integer.parseInt(campoInicio.getText());
+                            agregarTabla(1, aux1, aux2);
+                            
                         }
                     }
                     else
-                        r.setVisible(false);
+                        modelo.setRowCount(0);
                     
                 }catch(NumberFormatException ex){
                     JOptionPane.showMessageDialog(null, "Deben ser numeros", "Error", JOptionPane.ERROR_MESSAGE);
+                    modelo.setRowCount(0);
                 }
             }      
         };
@@ -179,7 +185,43 @@ public class Circulo extends JFrame{
         };
         regresar.addActionListener(evento3);
     }
+
     private double redondear(double number){
         return Math.round(number * 10000.0)/10000.0;
     }
+
+    private void agregarTabla(int number, int begin, int end){
+        modelo = new DefaultTableModel();
+        resultados =  new JTable(modelo);
+        if(number == 1){
+            modelo.addColumn("X");
+            modelo.addColumn("Y");
+            modelo.addColumn("r");
+            for(double i = begin; i <= end; i++){
+                Double res [] = {i,i, redondear(Math.sqrt(Math.pow(i, 2) + Math.pow(i, 2)))};
+                modelo.addRow(res);
+            }
+            panel.add(resultados);
+            JScrollPane scroll = new JScrollPane(resultados, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+            scroll.setBounds(150,120,250,150);
+            panel.add(scroll);
+        }
+        else{
+            modelo.addColumn("X");
+            modelo.addColumn("Y");
+            modelo.addColumn("r");
+            for(double i = begin; i <= end; i++){
+                Double res [] = {Math.pow(i - Double.parseDouble(H.getText()),2), Math.pow(i - Double.parseDouble(K.getText()),2), 
+                    redondear(Math.sqrt(Math.pow(i - Double.parseDouble(H.getText()),2) 
+                    + Math.pow(i - Double.parseDouble(K.getText()),2)))};
+                modelo.addRow(res);
+            }
+            panel.add(resultados);
+            JScrollPane scroll = new JScrollPane(resultados, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+            scroll.setBounds(150,120,250,150);
+            panel.add(scroll);
+        }
+        
+    } 
+    
 }
